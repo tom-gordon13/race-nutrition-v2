85f725295b6586c8462a774b0cd93b4f064447c6
tom-gordon13 <tombcgordon@gmail.com>
2025-04-20 09:21:57 -0600

    fixing stuff

--- ui/src/Home.tsx
+++ ui/src/Home.tsx
@@ -1,16 +1,34 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Container, Box } from '@mui/material';
 import Header from './components/Header';
 import FoodItemContainer from './components/FoodItemContainer';
 import RaceContainer from './components/RaceContainer';
+import SelectedItemNutrition from './components/SelectedItemNutrition';
 
 const Home: React.FC = () => {
+    const [isItemSelected, setIsItemSelected] = useState(false)
+    const [selectedItemNutritionExpanded, setSelectedItemNutritionExpanded] = useState(false)
+
+    const handleAccordionChange = () => {
+        setSelectedItemNutritionExpanded((prev) => isItemSelected ? !prev : false);
+
+    };
+
+    useEffect(() => {
+        setSelectedItemNutritionExpanded((prev) => isItemSelected ? !prev : false);
+    }, [isItemSelected])
+
     return (
         <Box sx={{ display: 'flex', flexDirection: 'column', minHeight: '100vh' }}>
             <Header />
-            <Box sx={{ mt: '10vh' }}> {/* Add margin-top to account for fixed header */}
+            <Box sx={{ mt: '10vh' }}>
                 <Container maxWidth="lg" sx={{ py: 3 }}>
-                    <FoodItemContainer />
+                    <FoodItemContainer setIsItemSelected={setIsItemSelected} />
+                    <SelectedItemNutrition
+                        expanded={selectedItemNutritionExpanded}
+                        isItemSelected={isItemSelected}
+                        onChange={handleAccordionChange}
+                    />
                     <RaceContainer />
                 </Container>
             </Box>
